a93a1f5544c6ec049335f8df0b13029f95c09a7c
Neil Pettinger <neil.pettinger@kurtosis.co.uk>
2025-03-07 17:09:53 +0000

    script_02.R (a day in the life of an ED)

---
+++ scripts/script_02.R
@@ -0,0 +1,155 @@
+
+# Case Study No. 2 - ED fullness minute by minute -------------------------
+
+
+# Step 0 - load the packages ----------------------------------------------
+
+library(readxl)
+library(tidyverse)
+library(ggpattern)
+library(colorspace)
+library(grid)
+
+# Step 1 - import the data ------------------------------------------------
+
+df_ed_day <-
+  read_xlsx("data/01_ed_duration_extract.xlsx") |> 
+  filter(arrival_datetime <= as.POSIXct('2015-03-07 23:59',
+                                        tz = "UTC")) |> 
+  filter(departure_datetime >= as.POSIXct('2015-03-07 00:00',
+                                          tz = "UTC"))
+
+
+# Step 2 - create a dataframe with 1441 minutes ---------------------------
+
+df_1441 <-
+  data_frame(date_hour_minute = seq(as.POSIXct('2015-03-07 00:00', tz = "UTC-1"),
+                                    as.POSIXct('2015-03-08 00:00', tz = "UTC-1"),
+                                    by = "1 min"))
+
+
+# Step 3 - do the non-equi join -------------------------------------------
+
+df_1441_a <-
+  df_1441 |> 
+  left_join(df_ed_day,
+            join_by(date_hour_minute >= arrival_datetime,
+                    date_hour_minute < departure_datetime)) |> 
+  group_by(date_hour_minute) |> 
+  summarize(ed_fullness = n())
+
+
+# Step 3a - calculate the minimum and maximum fullness --------------------
+
+min_fullness <- 
+  df_1441_a |> 
+  arrange(ed_fullness) |> 
+  mutate(seq_number = row_number()) |> 
+  filter(seq_number == 1) |> 
+  select(date_hour_minute)
+
+max_fullness <- 
+  df_1441_a |> 
+  arrange(desc(ed_fullness)) |> 
+  mutate(seq_number = row_number()) |> 
+  filter(seq_number == 1) |> 
+  select(date_hour_minute)
+
+
+# Step 4 - draw a graph ---------------------------------------------------
+
+ggplot(data = df_1441_a) +
+  aes(x = date_hour_minute,
+      y = ed_fullness) +
+  geom_area_pattern(pattern = "gradient",
+                    pattern_fill = "#fee6ce",
+                    pattern_fill2 = "#e6550d") +
+  geom_line(colour = "#e6550d") +
+  geom_segment(x = as.numeric(min_fullness),
+               y = 0,
+               xend = as.numeric(min_fullness),
+               yend = 26,
+               colour = "black",
+               linetype = "dashed") +
+  geom_segment(x = as.numeric(max_fullness),
+               y = 0,
+               xend = as.numeric(max_fullness),
+               yend = 39,
+               colour = "black",
+               linetype = "dashed") +
+  scale_x_datetime(date_labels = "%H %M") +
+  scale_y_continuous(limits = c(0, 40),
+                     breaks = seq(0, 40, 10)) +
+  annotate("text", 
+            x = as.POSIXct('2015-03-07 08:00'),
+            y = 25, 
+            label = "Minimum (2 patients in at 08:00)",
+            size = 2.5,
+            hjust = 1.17,
+            colour = "black") +
+  annotate("text", 
+           x = as.POSIXct('2015-03-07 15:10'),
+           y = 38, 
+           label = "Maximum (34 patients in at 15:10)",
+           size = 2.5,
+           hjust = 0.1,
+           colour = "black") +
+  labs(title = "A day in the life of an emergency department",
+       subtitle = "Minute-by-minute fullness snapshots: Sat 7 March 2015",
+       x = "",
+       y = "") +
+  theme_minimal() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
+
+
+# Step 6 - try Michael Duvekot's geom_area() solution ---------------------
+
+ggplot(data = df_1441_a) +
+  aes(x = date_hour_minute,
+      y = ed_fullness) +
+  geom_area(colour = colorspace::darken("#e76022", 10),
+            fill = grid::linearGradient(
+              x1 = 0, 
+              x2 = 0, 
+              y1 = 0, 
+              y2 = 40,
+              colours = c("#ffffff", "#e76022"))) +
+  #geom_line(colour = "#e6550d") +
+  geom_segment(x = as.numeric(min_fullness),
+               y = 0,
+               xend = as.numeric(min_fullness),
+               yend = 26,
+               colour = "black",
+               linetype = "dashed") +
+  geom_segment(x = as.numeric(max_fullness),
+               y = 0,
+               xend = as.numeric(max_fullness),
+               yend = 39,
+               colour = "black",
+               linetype = "dashed") +
+  scale_x_datetime(date_labels = "%H %M") +
+  scale_y_continuous(limits = c(0, 40),
+                     breaks = seq(0, 40, 10)) +
+  annotate("text", 
+           x = as.POSIXct('2015-03-07 08:00'),
+           y = 25, 
+           label = "Minimum (2 patients in at 08:00)",
+           size = 2.5,
+           hjust = 1.17,
+           colour = "black") +
+  annotate("text", 
+           x = as.POSIXct('2015-03-07 15:10'),
+           y = 38, 
+           label = "Maximum (34 patients in at 15:10)",
+           size = 2.5,
+           hjust = 0.1,
+           colour = "black") +
+  labs(title = "A day in the life of an emergency department",
+       subtitle = "Minute-by-minute fullness snapshots: Sat 7 March 2015",
+       x = "",
+       y = "") +
+  theme_minimal() +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_blank())
+
