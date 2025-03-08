9f94949de3bd341a584b4b3ed75f9beaea37eb5f
Neil Pettinger <neil.pettinger@kurtosis.co.uk>
2025-03-08 08:29:48 +0000

    Now with the import from a url

--- scripts/script_02.R
+++ scripts/script_02.R
@@ -4,19 +4,19 @@
 
 # Step 0 - load the packages ----------------------------------------------
 
-library(readxl)
+library(openxlsx)
 library(tidyverse)
 library(ggpattern)
-library(colorspace)
-library(grid)
+
 
 # Step 1 - import the data ------------------------------------------------
 
 df_ed_day <-
-  read_xlsx("data/01_ed_duration_extract.xlsx") |> 
-  filter(arrival_datetime <= as.POSIXct('2015-03-07 23:59',
+  read.xlsx("https://www.kurtosis.co.uk/data/01_ed_duration_extract.xlsx") |> 
+  mutate(new_arrival_datetime = convertToDateTime(arrival_datetime)) |> 
+  mutate(new_departure_datetime = convertToDateTime(departure_datetime)) |>filter(new_arrival_datetime <= as.POSIXct('2015-03-07 23:59',
                                         tz = "UTC")) |> 
-  filter(departure_datetime >= as.POSIXct('2015-03-07 00:00',
+  filter(new_departure_datetime >= as.POSIXct('2015-03-07 00:00',
                                           tz = "UTC"))
 
 
@@ -33,13 +33,13 @@ df_1441 <-
 df_1441_a <-
   df_1441 |> 
   left_join(df_ed_day,
-            join_by(date_hour_minute >= arrival_datetime,
-                    date_hour_minute < departure_datetime)) |> 
+            join_by(date_hour_minute >= new_arrival_datetime,
+                    date_hour_minute < new_departure_datetime)) |> 
   group_by(date_hour_minute) |> 
   summarize(ed_fullness = n())
 
 
-# Step 3a - calculate the minimum and maximum fullness --------------------
+# Step 4 - calculate the minimum and maximum fullness --------------------
 
 min_fullness <- 
   df_1441_a |> 
@@ -56,7 +56,7 @@ max_fullness <-
   select(date_hour_minute)
 
 
-# Step 4 - draw a graph ---------------------------------------------------
+# Step 5 - draw a graph ---------------------------------------------------
 
 ggplot(data = df_1441_a) +
   aes(x = date_hour_minute,
@@ -101,55 +101,3 @@ ggplot(data = df_1441_a) +
   theme_minimal() +
   theme(panel.grid.minor.x = element_blank(),
         panel.grid.major.x = element_blank())
-
-
-# Step 6 - try Michael Duvekot's geom_area() solution ---------------------
-
-ggplot(data = df_1441_a) +
-  aes(x = date_hour_minute,
-      y = ed_fullness) +
-  geom_area(colour = colorspace::darken("#e76022", 10),
-            fill = grid::linearGradient(
-              x1 = 0, 
-              x2 = 0, 
-              y1 = 0, 
-              y2 = 40,
-              colours = c("#ffffff", "#e76022"))) +
-  #geom_line(colour = "#e6550d") +
-  geom_segment(x = as.numeric(min_fullness),
-               y = 0,
-               xend = as.numeric(min_fullness),
-               yend = 26,
-               colour = "black",
-               linetype = "dashed") +
-  geom_segment(x = as.numeric(max_fullness),
-               y = 0,
-               xend = as.numeric(max_fullness),
-               yend = 39,
-               colour = "black",
-               linetype = "dashed") +
-  scale_x_datetime(date_labels = "%H %M") +
-  scale_y_continuous(limits = c(0, 40),
-                     breaks = seq(0, 40, 10)) +
-  annotate("text", 
-           x = as.POSIXct('2015-03-07 08:00'),
-           y = 25, 
-           label = "Minimum (2 patients in at 08:00)",
-           size = 2.5,
-           hjust = 1.17,
-           colour = "black") +
-  annotate("text", 
-           x = as.POSIXct('2015-03-07 15:10'),
-           y = 38, 
-           label = "Maximum (34 patients in at 15:10)",
-           size = 2.5,
-           hjust = 0.1,
-           colour = "black") +
-  labs(title = "A day in the life of an emergency department",
-       subtitle = "Minute-by-minute fullness snapshots: Sat 7 March 2015",
-       x = "",
-       y = "") +
-  theme_minimal() +
-  theme(panel.grid.minor.x = element_blank(),
-        panel.grid.major.x = element_blank())
-
